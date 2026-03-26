0ef79ffc88fae26dce6f5e15cc5c70f8c67e0c87
Louis Tan <louistan600@gmail.com>
2026-03-26 23:28:58 +0800

    add user model

---
+++ api/Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public byte[] PasswordHash { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = new byte[0];
+        public DateTime CreatedAt { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}

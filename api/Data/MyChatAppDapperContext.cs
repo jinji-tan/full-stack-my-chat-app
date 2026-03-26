05b455e58671ca0828f92cd2f0511dba29ca8f31
Louis Tan <louistan600@gmail.com>
2026-03-26 23:23:25 +0800

    setup dapper

---
+++ api/Data/MyChatAppDapperContext.cs
@@ -0,0 +1,38 @@
+using System.Data.Common;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace api.Data
+{
+    public class MyChatAppDapperContext
+    {
+        private readonly IConfiguration _config;
+        public MyChatAppDapperContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private DbConnection CreateConnection() => new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+        public async Task<IEnumerable<T>> LoadData<T>(string sql, object? parameter = null)
+        {
+            using var connection = CreateConnection();
+
+            return await connection.QueryAsync<T>(sql, parameter);
+        }
+
+        public async Task<T> LoadDataSingle<T>(string sql, object? parameter = null)
+        {
+            using var connection = CreateConnection();
+
+            return await connection.QuerySingleAsync<T>(sql, parameter);
+        }
+
+        public async Task<bool> ExecuteSql(string sql, object? parameter = null)
+        {
+            using var connection = CreateConnection();
+
+            return await connection.ExecuteAsync(sql, parameter) > 0;
+        }
+    }
+}

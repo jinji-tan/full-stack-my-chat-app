a5c10800cf25d585bb3d2936e4c3381828954acb
Louis Tan <louistan600@gmail.com>
2026-03-27 04:15:24 +0800

    setup

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb0697b31b0562c5d4d43fcceda282b6f9cefcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c28d8b30e5e883c23d49edc8e89512447385b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
